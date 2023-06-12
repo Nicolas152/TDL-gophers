b4059c3447d129fa5b6f83886f7c97aabc424fda
Luis Escalante <lescalante@fi.uba.ar>
2023-06-11 22:55:44 -0300

    Workspace controller refactor finished

--- src/controllers/workspace.go
+++ src/controllers/workspace.go
@@ -16,27 +16,7 @@ func AddWorkspaceController(myRouter *mux.Router) {
 	myRouter.HandleFunc("/gophers/workspace", authMiddleware.VerifyTokenMiddleware(workspaceHandler))
 
 	// Handler para obtener, actualizar y eliminar un workspace
-	myRouter.HandleFunc("/gophers/workspace/{workspaceKey}", func(writer http.ResponseWriter, r *http.Request) {
-		// Obtengo el workspaceKey de la URL
-		vars := mux.Vars(r)
-		workspaceKey := vars["workspaceKey"]
-
-		switch r.Method {
-		case http.MethodGet:
-			println("Get Workspace", workspaceKey)
-
-		case http.MethodPut:
-			println("Update Workspace", workspaceKey)
-
-		case http.MethodDelete:
-			println("Delete Workspace", workspaceKey)
-
-		default:
-			println("Method not allowed")
-		}
-
-		writer.Write([]byte("Workspace"))
-	})
+	myRouter.HandleFunc("/gophers/workspace/{workspaceKey}", authMiddleware.VerifyTokenMiddleware(workspaceHandlerByKey))
 }
 
 func workspaceHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,6 +40,28 @@ func workspaceHandler(w http.ResponseWriter, r *http.Request) {
 
 	case http.MethodPost:
 		println("Create Workspace")
+
+	default:
+		println("Method not allowed")
+	}
+
+	w.Write([]byte("Workspace"))
+}
+
+func workspaceHandlerByKey(w http.ResponseWriter, r *http.Request) {
+	// Obtengo el workspaceKey de la URL
+	vars := mux.Vars(r)
+	workspaceKey := vars["workspaceKey"]
+
+	switch r.Method {
+	case http.MethodGet:
+		println("Get Workspace", workspaceKey)
+
+	case http.MethodPut:
+		println("Update Workspace", workspaceKey)
+
+	case http.MethodDelete:
+		println("Delete Workspace", workspaceKey)
 
 	default:
 		println("Method not allowed")
